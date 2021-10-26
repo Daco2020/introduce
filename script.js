500cf7228748645cfe193b229424c75f2e0d93fc
Daco <dacokim32@gmail.com>
2021-10-27 00:16:26 +0900

    #1026

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-const todoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = todoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+// const todoForm = document.querySelector(".js-toDoForm"),
+//   toDoInput = todoForm.querySelector("input"),
+//   toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = "toDos";
+// const TODOS_LS = "toDos";
 
-let toDos = [];
+// let toDos = [];
 
-function deleteToDo(event) {
-  const btn = event.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function (toDo) {
-    // 필터 -> list 관련 //
-    return toDo.id !== parseInt(li.id);
-    // parseInt  는 스트링을 숫자로 바꿔준다//
-  });
-  toDos = cleanToDos;
-  saveToDos();
-}
+// function deleteToDo(event) {
+//   const btn = event.target;
+//   const li = btn.parentNode;
+//   toDoList.removeChild(li);
+//   const cleanToDos = toDos.filter(function (toDo) {
+//     // 필터 -> list 관련 //
+//     return toDo.id !== parseInt(li.id);
+//     // parseInt  는 스트링을 숫자로 바꿔준다//
+//   });
+//   toDos = cleanToDos;
+//   saveToDos();
+// }
 
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
+// function saveToDos() {
+//   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+// }
 
-function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleteToDo);
-  span.innerText = text;
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.id = newId;
-  toDoList.appendChild(li);
-  const toDoObj = {
-    text: text,
-    id: newId,
-  };
-  toDos.push(toDoObj);
-  saveToDos();
-}
+// function paintToDo(text) {
+//   const li = document.createElement("li");
+//   const delBtn = document.createElement("button");
+//   const span = document.createElement("span");
+//   const newId = toDos.length + 1;
+//   delBtn.innerText = "❌";
+//   delBtn.addEventListener("click", deleteToDo);
+//   span.innerText = text;
+//   li.appendChild(span);
+//   li.appendChild(delBtn);
+//   li.id = newId;
+//   toDoList.appendChild(li);
+//   const toDoObj = {
+//     text: text,
+//     id: newId,
+//   };
+//   toDos.push(toDoObj);
+//   saveToDos();
+// }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = "";
-}
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const currentValue = toDoInput.value;
+//   paintToDo(currentValue);
+//   toDoInput.value = "";
+// }
 
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function (toDo) {
-      //forEach -> array 관련 각각의 값을 불러올 수 있다
-      paintToDo(toDo.text);
-    });
-  }
-}
+// function loadToDos() {
+//   const loadedToDos = localStorage.getItem(TODOS_LS);
+//   if (loadedToDos !== null) {
+//     const parsedToDos = JSON.parse(loadedToDos);
+//     parsedToDos.forEach(function (toDo) {
+//       //forEach -> array 관련 각각의 값을 불러올 수 있다
+//       paintToDo(toDo.text);
+//     });
+//   }
+// }
 
-function init() {
-  loadToDos();
-  todoForm.addEventListener("submit", handleSubmit);
-}
+// function init() {
+//   loadToDos();
+//   todoForm.addEventListener("submit", handleSubmit);
+// }
 
-init();
+// init();
